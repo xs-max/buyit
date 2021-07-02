f6b03aacdb95b4b482eff3d33cb4ea2a845df29e
code_max_ <ogbujigodwinmax@gmail.com>
2021-07-02 20:49:54 +0100

    edited review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,51 +37,6 @@ reviewSchema.pre(/^find/, function(next) {
     next();
 } );
 
-reviewSchema.statics.calcAverageRatings = async function(tourId) {
-    const stats = await this.aggregate([
-        {
-            $match: {tour: tourId}
-        },
-        {
-            $group: {
-                _id: '$tour',
-                nRating: {$sum: 1},
-                averageRating: {$avg: '$rating'}
-            }
-        }
-    ]);
-
-    // console.log(stats);
-    if(stats.length > 0) {
-        await Tour.findByIdAndUpdate(tourId, {
-            ratingsQuantity: stats[0].nRating,
-            ratingsAverage: stats[0].averageRating
-        })
-    }else {
-        await Tour.findByIdAndUpdate(tourId, {
-            ratingsQuantity: 0,
-            ratingsAverage: 4.5
-        })
-    }
-    
-}
-
-reviewSchema.post('save', function(){
-    // This points to current review
-
-    this.constructor.calcAverageRatings(this.tour);
-    
-});
-
-reviewSchema.pre(/^findOneAnd/, async function(next) {
-    this.review = await this.findOne();
-    console.log(this.review);
-    next();
-});
-
-reviewSchema.post(/^findOneAnd/, async function() {
-    await this.review.constructor.calcAverageRatings(this.review.tour);
-});
 
 
 const Review = mongoose.model('Review', reviewSchema);
